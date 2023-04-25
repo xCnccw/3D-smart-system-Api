5f6247ec658796e78a021a51c60c7b0ef93aecea
xCnccw <1093933436@qq.com>
2023-04-25 14:00:40 +0800

    buildingdetails/list接口

--- router/buildings.js
+++ router/buildings.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
 // 导入用户路由处理函数模块
-const userHandler = require('../router_handler/buildings')
+const buildingsHandler = require('../router_handler/buildings')
 //获取车辆列表
-router.post('/buildings/list', userHandler.buildingslist)
+router.post('/buildings/list', buildingsHandler.buildingslist)
 //删除车辆
-router.post('/buildings/delete', userHandler.deletebuildings)
+router.post('/buildings/delete', buildingsHandler.deletebuildings)
 //增加车辆
-router.post('/buildings/add', userHandler.addbuildings)
+router.post('/buildings/add', buildingsHandler.addbuildings)
 //修改车辆信息
-router.post('/buildings/update', userHandler.updatebuildings)
+router.post('/buildings/update', buildingsHandler.updatebuildings)
 
 
 
